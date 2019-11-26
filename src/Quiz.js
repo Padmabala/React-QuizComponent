da0431a62ba4417a73a5f0b6cdae060cae530534
samlin <samlin.wilson@accenture.com>
2019-11-26 16:10:43 +0530

    No big change

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,8 +13,6 @@ class Quiz extends Component {
     };
   }
   render() {
-    const isQuizEnd =
-      this.state.quiz_position - 1 === quizData.quiz_questions.length;
     const { quiz_position } = this.state;
     const isQuizEnd = quiz_position - 1 === quizData.quiz_questions.length;
     return (
