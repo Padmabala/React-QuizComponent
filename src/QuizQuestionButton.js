5afbcad73cb091958c8955a210c7b9c72524481e
samlin <samlin.wilson@accenture.com>
2019-11-26 15:16:27 +0530

    Added QuizQuestionButton Component

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+class QuizQuestionButton extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>
+            <button>{this.props.button_text}</button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default QuizQuestionButton;
