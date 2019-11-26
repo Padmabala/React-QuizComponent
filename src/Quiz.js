ad077f0f3c21e91ccdf196fd0fe4b550ab473920
samlin <samlin.wilson@accenture.com>
2019-11-26 16:06:54 +0530

    Added condition to check end of quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,15 +13,19 @@ class Quiz extends Component {
     };
   }
   render() {
+    const isQuizEnd =
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
     const { quiz_position } = this.state;
     const isQuizEnd = quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
-        <QuizEnd />
-
-        <QuizQuestion
-          quiz_question={quizData.quiz_questions[quiz_position - 1]}
-        ></QuizQuestion>
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={quizData.quiz_questions[quiz_position - 1]}
+          ></QuizQuestion>
+        )}
       </div>
     );
   }
